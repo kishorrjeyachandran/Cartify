78e748f13554984b3e733f77fd19c5816b9aca59
rjehhhh <kishor.r.jeyachandran@gmail.com>
2026-05-08 20:52:21 +0530

    Fixed production CORS

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,7 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: "https://cartify-v2ua.onrender.com",
+    origin: "https://cartify-project-o108ia9k1-kishorrjeyachandrans-projects.vercel.app/",
     credentials: true,
   })
 );
